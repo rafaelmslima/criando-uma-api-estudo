34efeb517b986b9ebf2f4660990f2c636a68fbcb
Rafael Lima <raafaelmslima@gmail.com>
2025-02-26 16:54:09 -0300

    Refatorando o controlador de livro e ajustando erros

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,16 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser, // Mantém o suporte ao ambiente do navegador
+        ...globals.node // Adiciona suporte ao ambiente Node.js
+      },
+      sourceType: "module", // Se estiver usando ES Modules
+    },
+  },
   pluginJs.configs.recommended,
 ];
--- scr/controllers/autoresController.js
+++ scr/controllers/autoresController.js
@@ -1,4 +1,5 @@
 import { autor } from "../models/autores.js";
+import mongoose from "mongoose";
 
 class AutorController {
   static async listarAutores (req, res) {
@@ -13,10 +14,27 @@ class AutorController {
   static async listarAutorPorId (req, res) {
     try {
       const id = req.params.id;
+
       const autorEncontrado = await autor.findById(id);
-      res.status(200).json(autorEncontrado);
+
+      if (autorEncontrado !== null) {
+        res.status(200).json(autorEncontrado);
+      }
+      else {
+      res.status(404).json({message: "Id do Autor não localizado"});
+        
+      }
     } catch(erro) {
-      res.status(500).json({message: `${erro.message} - Não foi possível encontrar esse autor`});
+      if (erro instanceof mongoose.Error.CastError) {
+        
+        res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos"});
+
+      }
+      else {
+
+      res.status(500). send({message: "Erro interno de Servidor"});
+      
+      }
     }
   };
 
